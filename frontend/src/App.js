2de22b20599146ed45f4ee897a9964babed407ac
ayoubjdad <ayoubjdad1@gmail.com>
2023-03-17 21:42:12 +0100

    feat: update app file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,32 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Home from "./pages/Home";
 import Header from "./components/Header/Header";
-import HomeSlider from "./components/HomeSlider/HomeSlider";
-import Slider from "./components/Slider/Slider";
+import Footer from "./components/Footer/Footer";
+import Catalogue from "./pages/Catalogue";
+import Contact from "./pages/Contact";
+
+const colorPalette = {
+  black: "black",
+  lightGreen: "#49e295",
+  hardGreen: "#1f2d27",
+  lightGray: "#edf0ef",
+  hardGray: "#c7cec9",
+};
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <HomeSlider />
-      <Slider />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="catalogue" element={<Catalogue />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
